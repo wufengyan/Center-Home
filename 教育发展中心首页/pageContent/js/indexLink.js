d9077f5a57cab42b0cbde464e0e7dad4b3a2b9c5
wufengyan <2875212677@qq.com>
2020-01-05 18:06:11 +0800

    first commit

---
+++ 教育发展中心首页/pageContent/js/indexLink.js
@@ -0,0 +1,40 @@
+// class="div-link"
+document.write('<div style="background:url(images/lianjie-background.gif) no-repeat;height:32px;"  align="center" valign="middle">');
+document.write('<a style="background:url(images/alllogo.gif) no-repeat;white-space:nowrap;width:105px;height:32px;float:left; white-space:nowrap;" href="#" title="友情链接" ></a>');
+// class="PContentSelect"
+document.write('<SELECT style="margin: 8px 0 0 35px;float: left;line-height: 31px;" name="select"> ');
+document.write('<OPTION>国家教育部门链接</OPTION> ');
+document.write('<OPTION value="http://www.moe.edu.cn">中国教育部</OPTION> ');
+document.write('<OPTION value="http://www.cbe21.com">中国基础教育网</OPTION> ');
+document.write('<OPTION value="http://www.mii.gov.cn">中华人民共和国信息产业部</OPTION> ');
+document.write('<OPTION value="http://www.edu.cn">中国教育科研网</OPTION> ');
+document.write('<OPTION value="http://www.cbern.gov.cn">国家基础教育资源网</OPTION> ');
+document.write('<OPTION value="http://www.nettime.net.cn/itedu">《信息技术教育》杂志</OPTION>');
+document.write('</SELECT>');
+// class="PContentSelect"
+document.write('<SELECT   style=" margin: 8px 0 0 35px;float: left;line-height: 31px;"name="select2"> ');
+document.write('<OPTION>省教育部门链接</OPTION> ');
+document.write('<OPTION value="http://www.gdhed.edu.cn">广东省教育厅</OPTION> ');
+document.write('<OPTION value="http://www.eeagd.edu.cn">广东省考试中心</OPTION> ');
+document.write('<OPTION value="http://www.gdedu123.com">广东基础教育信息资源中心</OPTION> ');
+document.write('<OPTION value="http://zyh.fsjy.net">广东省第九届中学生运动会</OPTION>');
+document.write('</SELECT>');
+// class="PContentSelect"
+document.write('<SELECT  style="margin: 8px 0 0 35px;float: left;line-height: 31px;" name="select3"> ');
+document.write('<OPTION>市教育部门链接</OPTION> ');
+document.write('<OPTION value="http://www.fsjy.net">佛山教育信息港</OPTION> ');
+document.write('<OPTION value="http://www.ccjy.cn">禅城教育信息网</OPTION> ');
+document.write('<OPTION value="http://www.nhedu.net">南海教育信息网</OPTION> ');
+document.write('<OPTION value="http://jy.ss.gov.cn">三水教育信息网</OPTION> ');
+document.write('<OPTION value="http://www.gmedu.org">高明教育信息网</OPTION> ');
+document.write('<OPTION value="http://www.fszj.net">佛山支教在线</OPTION>');
+document.write('</SELECT>');
+// class="PContentSelect"
+document.write('<SELECT  style="margin: 8px 0 0 35px;float: left;line-height: 31px;" name="select3" name="friendLink1"> ');
+document.write('<OPTION>区内网站链接</OPTION> ');
+document.write('<OPTION value="http://www.shunde.gov.cn">顺德政务网</OPTION> ');
+document.write('<OPTION value="http://www.shunde.net.cn">顺德信息网</OPTION> ');
+document.write('<OPTION value="http://www.sdpt.com.cn">顺德职业技术学院</OPTION>');
+document.write('<OPTION value="http://www.shundecity.com">顺德城市网</OPTION>');
+document.write('</SELECT>');
+document.write('</div>');
